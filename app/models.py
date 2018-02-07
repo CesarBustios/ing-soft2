84cbce7e2856e420b1e4eb893140548e5d7d35a4
César Bustíos <cesarbustios@gmail.com>
2018-02-07 11:05:56 -0500

    Models and migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+import math
 from django.db import models
 
-# Create your models here.
+
+PUNTAJE_MINIMO = 85
+PUNTAJE_MAXIMO = 100
+
+
+class Seleccion(models.Model):
+    pais = models.CharField(max_length=50)
+
+    class Meta:
+        managed = True
+        db_table = 'selecciones'
+
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=200)
+    idpais = models.ForeignKey('Seleccion', on_delete=models.CASCADE)
+
+    @property
+    def puntaje(self):
+        return random.randint(PUNTAJE_MINIMO, PUNTAJE_MAXIMO)
+
+    class Meta:
+        managed = True
+        db_table = 'jugadores'
