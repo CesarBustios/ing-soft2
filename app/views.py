916ab93c2ca31218236465f901e3c4ac2e290be3
César Bustíos <cesarbustios@gmail.com>
2018-02-07 11:50:13 -0500

    Context data for tempaltes

--- app/views.py
+++ app/views.py
@@ -4,8 +4,16 @@ from django.views.generic.base import TemplateView
 
 
 class EquipoView(TemplateView):
-    pass
+    template_name = 'app/equipo.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(EquipoView, self).get_context_data(*args, **kwargs)
+        return context
 
 
 class ResultadoView(TemplateView):
-    pass
+    template_name = 'app/resultado.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ResultadoView, self).get_context_data(*args, **kwargs)
+        return context
