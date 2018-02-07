9801e297d03ddd6540472c01de6cbdc10bd0295d
César Bustíos <cesarbustios@gmail.com>
2018-02-07 10:35:59 -0500

    Using postgresql

--- rusia_site/settings.py
+++ rusia_site/settings.py
@@ -83,8 +83,13 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webappdb',
+        'USER': 'alfonso',
+        'PASSWORD': 'alfonso',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 DATABASES['default'].update(db_from_env)
