b8ddbd1307053fb90e5d6ba74dbac5bf565eacca
César Bustíos <cesarbustios@gmail.com>
2018-02-08 12:41:30 -0500

    Debug false

--- rusia_site/settings.py
+++ rusia_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*if#92^ec$var#lp2!edbw*fytfwhn(q_tff8rbj7oz1*py203'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
