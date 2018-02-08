d70fb0179fb2026e69959c3811b5528201129b40
César Bustíos <cesarbustios@gmail.com>
2018-02-08 10:36:06 -0500

    Processing the POST

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic.base import TemplateView
 
 from .forms import EquipoForm
+from .models import Jugador
 
 
 class EquipoView(TemplateView):
@@ -12,6 +14,18 @@ class EquipoView(TemplateView):
         context = super(EquipoView, self).get_context_data(*args, **kwargs)
         context['form'] = EquipoForm()
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = EquipoForm(request.POST)
+        if form.is_valid():
+            # Extraemos la informacion solo los campos que nos interesan
+            resultado = 0
+            ids = [v for k, v in form.cleaned_data.items() if k not in ['jugadores', 'selecciones', 'posiciones']]
+            jugadores = Jugador.objects.filter(id__in=ids)
+            for jugador in jugadores:
+                print(jugador.puntaje)
+                resultado += jugador.puntaje
+        return reverse('resultado', kwargs={'puntaje': resultado})
 
 
 class ResultadoView(TemplateView):
