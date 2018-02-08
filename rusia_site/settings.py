33a5d0da7493cf7a80bc4d40a258d5a83d4bbfb0
César Bustíos <cesarbustios@gmail.com>
2018-02-08 14:50:28 -0500

    Adding whitenoise middleware

--- rusia_site/settings.py
+++ rusia_site/settings.py
@@ -51,12 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-]
-
-MIDDLEWARE_CLASSES = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
-)
+]
 
 ROOT_URLCONF = 'rusia_site.urls'
 
