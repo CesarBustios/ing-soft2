58f93b91913759e8cdabaa97f1e06af1e979c569
César Bustíos <cesarbustios@gmail.com>
2018-02-07 22:46:47 -0500

    More fields and attrs

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
+from .models import Seleccion, Jugador
+
 
 class EquipoForm(forms.Form):
-    POSICION_CHOICES = (
+    POSICIONES_CHOICES = (
         ('arquero', 'ARQUERO'),
         ('defensa1', 'DEFENSA 1'),
         ('defensa2', 'DEFENSA 2'),
@@ -16,5 +18,26 @@ class EquipoForm(forms.Form):
         ('delantero2', 'DELANTERO 2'),
         ('delantero3', 'DELANTERO 3'),
     )
-    posicion = forms.ChoiceField(choices=POSICION_CHOICES, widget=forms.RadioSelect)
+    posiciones = forms.ChoiceField(choices=POSICIONES_CHOICES, widget=forms.RadioSelect)
     arquero = forms.IntegerField(widget=forms.HiddenInput())
+    defensa1 = forms.IntegerField(widget=forms.HiddenInput())
+    defensa2 = forms.IntegerField(widget=forms.HiddenInput())
+    defensa3 = forms.IntegerField(widget=forms.HiddenInput())
+    defensa4 = forms.IntegerField(widget=forms.HiddenInput())
+    centro1 = forms.IntegerField(widget=forms.HiddenInput())
+    centro2 = forms.IntegerField(widget=forms.HiddenInput())
+    centro3 = forms.IntegerField(widget=forms.HiddenInput())
+    delantero1 = forms.IntegerField(widget=forms.HiddenInput())
+    delantero2 = forms.IntegerField(widget=forms.HiddenInput())
+    delantero3 = forms.IntegerField(widget=forms.HiddenInput())
+    selecciones = forms.ModelChoiceField(queryset=Seleccion.objects.all().order_by('pais'))
+    jugadores = forms.ModelChoiceField(queryset=Jugador.objects.all().order_by('nombre'))
+
+    def __init__(self, *args, **kwargs):
+        super(EquipoForm, self).__init__(*args, **kwargs)
+        self.fields['selecciones'].widget.attrs = {
+            'class': 'form-control'
+        }
+        self.fields['jugadores'].widget.attrs = {
+            'class': 'form-control'
+        }
