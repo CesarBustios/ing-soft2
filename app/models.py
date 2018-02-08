6359c0aacf75904b287b27d9d834ce56a5e48ad2
César Bustíos <cesarbustios@gmail.com>
2018-02-07 22:30:00 -0500

    String representation for models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Seleccion(models.Model):
     pais = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.pais
+
     class Meta:
         managed = True
         db_table = 'selecciones'
@@ -15,6 +18,9 @@ class Jugador(models.Model):
     nombre = models.CharField(max_length=200)
     pais = models.ForeignKey('Seleccion', on_delete=models.CASCADE)
     puntaje = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
 
     class Meta:
         managed = True
