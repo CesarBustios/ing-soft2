b0335b8883fbe5a56dee5621a58e4632a1bbb4d0
César Bustíos <cesarbustios@gmail.com>
2018-02-07 22:09:44 -0500

    Initial form and some CSS

--- app/views.py
+++ app/views.py
@@ -2,12 +2,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
+from .forms import EquipoForm
+
 
 class EquipoView(TemplateView):
     template_name = 'app/equipo.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(EquipoView, self).get_context_data(*args, **kwargs)
+        context['form'] = EquipoForm()
         return context
 
 
